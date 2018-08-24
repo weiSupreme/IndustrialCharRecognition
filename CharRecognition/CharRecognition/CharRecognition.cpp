9131efcb49f15a42d349a4200c38d70002937325
weiSupreme <zxdzhuwei@foxmail.com>
2018-08-24 18:57:53 +0800

    得到旋转图像

--- CharRecognition/CharRecognition/CharRecognition.cpp
+++ CharRecognition/CharRecognition/CharRecognition.cpp
@@ -23,29 +23,57 @@ int _tmain(int argc, _TCHAR* argv[])
 	
 	MyImgProc *mip = new MyImgProc();
 
+	//预处理：滤波，阈值分割
 	Mat emphasizeImg;
 	mip->Emphasize(srcImg, &emphasizeImg, 7, 7, 5);
 	Mat binaryImg;
 	threshold(emphasizeImg, binaryImg, 190, 255, 1);
 
+	//形态学处理
 	Mat morphologyImg;
 	mip->Morphology(binaryImg, &morphologyImg);
 
+	//寻找文字区域
 	vector<RotatedRect> rects;
 	mip->FindTextRegion(morphologyImg, &rects);
 
 	//画出轮廓
-	for each (RotatedRect rect in rects)
+	if (0)
 	{
-		Point2f P[4];
-		rect.points(P);
-		for (int j = 0; j <= 3; j++)
+		for each (RotatedRect rect in rects)
 		{
-			line(srcImg, P[j], P[(j + 1) % 4], Scalar(0, 255, 0), 2);
+			Point2f P[4];
+			rect.points(P);
+			for (int j = 0; j <= 3; j++)
+			{
+				line(srcImg, P[j], P[(j + 1) % 4], Scalar(0, 255, 0), 2);
+			}
 		}
 	}
 
-	imshow("img", srcImg);
+	//计算文本区域旋转角度
+	//cout << rects.size() << endl;
+	float angle=0.0;
+	if (rects.size() != 1)
+	{
+		cout << "找到"<<rects.size()<<"个文本区域" << endl;
+		return 0;
+	}
+	else
+	{
+		angle = rects[0].angle;
+		if (0 < abs(angle) && abs(angle) <= 45)  //逆时针
+			angle = angle;
+		else if (45 < abs(angle) && abs(angle) < 90) //顺时针
+			angle = 90 - abs(angle);
+	}
+
+	//旋转图像，得到水平文字
+	Mat rotatedImg;
+	Mat M = getRotationMatrix2D(rects[0].center, angle, 1);
+	warpAffine(srcImg, rotatedImg, M, srcImg.size());
+
+	imshow("img", rotatedImg);
 	//imwrite("D:/实习/图片/pic/2017.08.25/C1-08251718/a1_0.bmp", DstImg);
 	waitKey(0);
 
